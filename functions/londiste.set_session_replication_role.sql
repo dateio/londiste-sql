bb234e5502b11f0374edabffe6cc730bbf18d948
Marko Kreen <markokr@gmail.com>
2019-05-21 22:29:22 +0300

    Restore set_session_replication_role func

--- functions/londiste.set_session_replication_role.sql
+++ functions/londiste.set_session_replication_role.sql
@@ -1,7 +1,7 @@
 
 create or replace function londiste.set_session_replication_role(val text, is_local bool) returns void as $$
 begin
+    perform set_config('session_replication_role', val, is_local);
 end;
 $$ language plpgsql security definer;
 
-grant execute on function londiste.set_session_replication_role(text, bool) to londiste_writer;
