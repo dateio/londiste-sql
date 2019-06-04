6d7e8f9cd10a3516ecae002fbf170ffe9aa3576c
Marko Kreen <markokr@gmail.com>
2019-06-04 09:26:48 +0300

    Make londiste.set_session_replication_role compat with RDS

--- functions/londiste.set_session_replication_role.sql
+++ functions/londiste.set_session_replication_role.sql
@@ -1,7 +1,27 @@
 
 create or replace function londiste.set_session_replication_role(val text, is_local bool) returns void as $$
 begin
-    perform set_config('session_replication_role', val, is_local);
+    if is_local then
+        if val = 'origin' then
+            set local session_replication_role = 'origin';
+        elsif val = 'replica' then
+            set local session_replication_role = 'replica';
+        elsif val = 'local' then
+            set local session_replication_role = 'local';
+        else
+            raise exception 'bad value for session_replication_role';
+        end if;
+    else
+        if val = 'origin' then
+            set session_replication_role = 'origin';
+        elsif val = 'replica' then
+            set session_replication_role = 'replica';
+        elsif val = 'local' then
+            set session_replication_role = 'local';
+        else
+            raise exception 'bad value for session_replication_role';
+        end if;
+    end if;
 end;
 $$ language plpgsql security definer;
 
